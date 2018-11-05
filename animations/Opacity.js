8702cdbdd41997a8c7cab9805ded104eefd249c9
tinghaoma <tinghaoma@sohu-inc.com>
2018-11-05 21:15:09 +0800

    Opacity 透明度动画

---
+++ animations/Opacity.js
@@ -0,0 +1,68 @@
+/**
+ * @author itck_mth
+ * @time 2018/11/5 21:01
+ * @class describe
+ */
+
+
+import React, {Component} from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    Animated,
+    Image,
+    StyleSheet,
+    Easing,
+} from 'react-native';
+
+
+export default class Opacity extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            fadeOutOpacity: new Animated.Value(1),
+        }
+
+        this.fadeOutAnimated = Animated.timing(
+            this.state.fadeOutOpacity,
+            {
+                toValue: 0,  //透明度动画最终值
+                duration: 3000,   //动画时长3000毫秒
+                easing: Easing.linear,
+            }
+        );
+    }
+
+    _startAnimated() {
+        this.fadeOutAnimated.start(() => this.state.fadeOutOpacity.setValue(1));
+    }
+
+    render() {
+        return (
+            <View style={styles.mainStyle}>
+                <Animated.View style={{width: 200, height: 300, opacity: this.state.fadeOutOpacity}}>
+                    <Image ref="image" style={{width: 200, height: 300}}
+                           source={require('../res/imgs/lyf.jpeg')}>
+                    </Image>
+                </Animated.View>
+
+                <TouchableOpacity style={styles.touchStyle} onPress={this._startAnimated.bind(this)}>
+                    <Text style={{width: 200, height: 100, textAlign: 'center', lineHeight: 100}}>点击开始动画</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    mainStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    touchStyle:{
+        backgroundColor:'gray',
+    }
+})
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import {AppRegistry} from 'react-native';
 import RequestAnimationDemo from './animations/RequestAnimationDemo';
 import LayoutAnimationDemo from './animations/LayoutAnimationDemo';
+import Opaticy from './animations/Opacity';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => LayoutAnimationDemo);
+AppRegistry.registerComponent(appName, () => Opaticy);
