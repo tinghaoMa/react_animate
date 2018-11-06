ee3e9b7ab10898985a0b1c58f1123ec54bdc926c
tinghaoma <tinghaoma@sohu-inc.com>
2018-11-06 15:54:22 +0800

    setState 练习

--- animations/Opacity.js
+++ animations/Opacity.js
@@ -24,6 +24,8 @@ export default class Opacity extends Component {
 
         this.state = {
             fadeOutOpacity: new Animated.Value(1),
+            name: '',
+            count:0,
         }
 
         this.fadeOutAnimated = Animated.timing(
@@ -38,10 +40,14 @@ export default class Opacity extends Component {
 
     componentDidMount() {
 
-        InteractionManager.runAfterInteractions(() => {
-            console.log('hello world');
-        })
-        this._startAnimated();
+        // InteractionManager.runAfterInteractions(() => {
+        //     console.log('hello world');
+        // })
+        // this._startAnimated();
+
+        this.test();
+        this.test2();
+
     }
 
     _startAnimated() {
@@ -49,6 +55,7 @@ export default class Opacity extends Component {
     }
 
     render() {
+        console.log('render');
         return (
             <View style={styles.mainStyle}>
                 <Animated.View style={{width: 200, height: 300, opacity: this.state.fadeOutOpacity}}>
@@ -60,8 +67,52 @@ export default class Opacity extends Component {
                 <TouchableOpacity style={styles.touchStyle} onPress={this._startAnimated.bind(this)}>
                     <Text style={{width: 200, height: 100, textAlign: 'center', lineHeight: 100}}>点击开始动画</Text>
                 </TouchableOpacity>
+
+                <Text style={{fontSize: 20, color: 'red'}}>{this.state.name}</Text>
+                <Text style={{fontSize: 20, color: 'red'}}>{this.state.count}</Text>
+
             </View>
         );
+    }
+
+    componentWillUpdate() {
+        console.log('componentWillUpdate')
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate')
+    }
+
+    test2 = () => {
+        //异步
+        // this.setState({count: this.state.count + 1});
+        // this.setState({count: this.state.count + 1});
+
+        //同步
+        this.setState((prevState, props) => ({
+            count: prevState.count + 1
+        }));
+        this.setState((prevState, props) => ({
+            count: prevState.count + 1
+        }));
+    }
+
+    test = () => {
+        console.log('setState');
+        this.setState({
+            name: '呵呵'
+        }, () => {
+            console.log('1111 = ' + this.state.name);
+        })
+        console.log('2222 = ' + this.state.name);
+        setTimeout(() => {
+            this.setState({
+                name: '呵呵11111'
+            }, () => {
+                console.log('3333333 = ' + this.state.name);
+            })
+            console.log('444444 = ' + this.state.name);
+        }, 1000);
     }
 }
 
